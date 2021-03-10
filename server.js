6f0f7498c55a4bbb633618c37f2d1e59a2f4f058
darlanquimas <darlanquimas@gmail.com>
2021-03-10 15:08:19 -0300

    Separação de arquivo server e app

--- server.js
+++ server.js
@@ -1,27 +1,17 @@
-"use strict";
-
-const http = require("http");
+const app = require("./src/app");
 const debug = require("debug")("apirest:server");
-const express = require("express");
+const http = require("http");
 
-const app = express();
 const port = normalizePort(process.env.PORT || 5000);
+
 app.set("port", port);
 
 const server = http.createServer(app);
-const router = express.Router();
-
-const route = router.get("/", (req, res, bext) => {
-  res.status(200).send({
-    title: "Node Api",
-    version: "0.0.1",
-  });
-});
-
-app.use("/", router);
 
 server.listen(port);
 server.on("error", onError);
+server.on("listening", onListening);
+console.log("Api rodando na porta " + port);
 
 function normalizePort(val) {
   const port = parseInt(val, 10);
@@ -53,4 +43,10 @@ function onError(error) {
     default:
       throw error;
   }
+}
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("listening on " + bind);
 }
